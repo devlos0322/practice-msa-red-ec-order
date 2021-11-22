4d3f52c9553f8b51b6ba68f7da63bfcfbd9e4dcb
devlos0322 <devlos0322@gmail.com>
2021-11-22 18:08:44 +0900

    Ｉｎｉｔ

---
+++ src/main/java/org/devlos/practicemsaredecorder/PracticeMsaRedEcOrderApplication.java
@@ -0,0 +1,13 @@
+package org.devlos.practicemsaredecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMsaRedEcOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMsaRedEcOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devlos/practicemsaredecorder/PracticeMsaRedEcOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devlos.practicemsaredecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMsaRedEcOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
